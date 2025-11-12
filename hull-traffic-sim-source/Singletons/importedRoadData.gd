5991ff2b5cb89b193bd8b4ff01d4d6f7d3a86fb9
Tyler Rigg <T.RIGG-2021@hull.ac.uk>
2025-11-12 16:27:17 +0000

    The importer script now splits the data between Nodes and Ways for easier handling of the data

--- hull-traffic-sim-source/Singletons/importedRoadData.gd
+++ hull-traffic-sim-source/Singletons/importedRoadData.gd
@@ -3,12 +3,18 @@ extends Node
 # This script gets the Import Data for roads from OSM and turns it into a singleston that sits under the root, so the road data can be used later.
 # It also transforms the data into a better format for the simulator to use
 
-var roadData = {}
+var nodeData = {}
+var waysData = {}
+var finishedImporting = false
 
 var pathToRoadData = "res://OverpassAPIOSMData/TestOSMImportData1.json"
 
 func _ready():
-	roadData = loadJsonFile(pathToRoadData)
+	var data = loadJsonFile(pathToRoadData)
+	nodeData = data[0]
+	waysData = data[1]
+	finishedImporting = true
+
 
 func loadJsonFile(filePath : String):
 	if FileAccess.file_exists(filePath):
@@ -16,7 +22,22 @@ func loadJsonFile(filePath : String):
 		var parsedResult = JSON.parse_string(dataFile.get_as_text())
 		
 		if parsedResult is Dictionary:
-			return parsedResult
+			var parsedNodes = {}
+			var parsedWays = {}
+			
+			# Removing unnecassary data
+			parsedResult.erase("version")
+			parsedResult.erase("generator")
+			parsedResult.erase("osm3s")
+			
+			for object in parsedResult.elements:	
+				if object.type == "node":
+					parsedNodes[object.id] = object.duplicate(true)
+						
+				elif object.type == "way":
+					parsedWays[object.id] = object.duplicate(true)
+			
+			return [parsedNodes, parsedWays]
 		else:
 			print("Error reading File")
 	else:
